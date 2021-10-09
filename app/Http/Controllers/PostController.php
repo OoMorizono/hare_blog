c91727e909d26c8a60de285e3eaefb2c34d2ce61
OoMorizono <x65f588d72sw76@gmail.com>
2021-10-09 11:46:17 +0900

    index,createの作成

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\post;
+use App\Http\Requests\PostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\PostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\PostRequest  $request
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(post $post)
+    {
+        //
+    }
+}
