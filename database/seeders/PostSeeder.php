728ea75114b6fe5a2213654fb1609bff83663f3a
OoMorizono <x65f588d72sw76@gmail.com>
2021-10-10 15:07:15 +0900

    追加機能

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PostSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\Post::factory(1)->create();
+        \App\Models\Post::factory(20)->create();
     }
 }
